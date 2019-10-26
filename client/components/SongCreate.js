7fdca1b45463979be98afbcacba7d21777771cb8
superlucky84 <superlucky84@gmail.com>
2019-10-27 02:20:18 +0900

    first commit

---
+++ client/components/SongCreate.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create a New Song</h3>
+        <label>
+          input:
+        </label>
+        <input 
+          onChange={event => this.setState({title: event.target.value})} 
+          value={this.state.title}
+        />
+
+      </div>
+    );
+  }
+}
+
+export default SongCreate;
---
+++ client/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {ApolloProvider} from 'react-apollo';
+import ApolloClient from 'apollo-client';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+import App from './components/App';
+import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
+
+const client = new ApolloClient({});
+
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+        <Route path="/song/new" component={SongCreate}>
+        </Route>
+      </Router>
+    </ApolloProvider>
+  )
+};
+
+ReactDOM.render(
+  <Root />,
+  document.querySelector('#root')
+);

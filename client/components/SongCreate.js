fcd90377d6e336797592708561fc35d1661d2eef
superlucky84 <superlucky84@gmail.com>
2019-10-29 01:11:41 +0900

    ing

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import gql from 'graphql-tag'
+import {graphql} from 'react-apollo';
+import {Link, hashHistory} from 'react-router';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -9,19 +12,39 @@ class SongCreate extends Component {
     }
   }
 
+  onSubmit(event) {
+    event.preventDefault();
+
+    this.props.mutate({
+      variables: {
+        title: this.state.title
+      }
+    }).then(() => hashHistory.push('/'));
+  }
+
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
-        <label>Song Title:</label>
-        <input 
-          onChange={event => this.setState({title: event.target.value})} 
-          value={this.state.title}
-        />
-
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>Song Title:</label>
+          <input 
+            onChange={event => this.setState({title: event.target.value})} 
+            value={this.state.title}
+          />
+        </form>
       </div>
     );
   }
 }
 
-export default SongCreate;
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
+    }
+  }
+`
+
+export default graphql(mutation)(SongCreate);

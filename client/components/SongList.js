f9456ee82e33a5c3c82cf81852de5e61f020e34a
superlucky84 <superlucky84@gmail.com>
2019-11-05 12:58:01 +0900

    middle commit

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,23 +1,33 @@
 import React, {Component} from 'react';
-import gql from 'graphql-tag'
+import gql from 'graphql-tag';
 import {graphql} from 'react-apollo';
 import {Link} from 'react-router';
 import query from '../queries/fetchSongs';
 
 
 class SongList extends Component {
+  onSongDelete(id) {
+    this.props.mutate({ variables: { id } })
+    .then(() => this.props.data.refetch());
+  }
+
   renderSongs() {
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({id, title}) => {
       return (
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          {title}
+          <i
+            className="material-icons"
+            onClick={() => this.onSongDelete(id)}
+          >
+            delete
+          </i>
         </li>
       );
     });
   }
 
   render() {
-    console.log(this.props.data);
     if (this.props.data.loading) {
       return <div>Loading...</div>;
     }
@@ -38,4 +48,14 @@ class SongList extends Component {
   }
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }`;
+
+export default graphql(mutation)(
+  graphql(query)(SongList)
+);
+
--- client/index.js
+++ client/index.js
@@ -1,3 +1,4 @@
+import './style/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {ApolloProvider} from 'react-apollo';
@@ -6,6 +7,7 @@ import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 import App from './components/App';
 import SongList from './components/SongList';
 import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail';
 
 const client = new ApolloClient({});
 
@@ -17,8 +19,8 @@ const Root = () => {
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
         </Route>
-        <Route path="/songs/new" component={SongCreate}>
-        </Route>
+        <Route path="/songs/new" component={SongCreate} />
+        <Route path="/songs/:id" component={SongDetail} />
       </Router>
     </ApolloProvider>
   )

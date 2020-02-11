99a513671d7fbed373d597afca7d7af6bfa7a832
superlucky84 <superlucky84@gmail.com>
2020-02-11 12:59:34 +0900

    ing chapter 61

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
+import {graphql} from 'react-apollo';
+import {Link} from 'react-router';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
+    console.log(this.props);
+    const {song} = this.props.data;
+
+    if (!song) { return <div>loading...</div>; }
+
     return (
       <div>
-        <h3>Song Detail</h3>
+        <Link to="/">Back</Link>
+        <h3>{song.title}</h3>
       </div>
     );
   }
 }
 
 
-export default SongDetail;
+export default graphql(fetchSong, {
+    options: (props) => {return {variables: {id: props.params.id}}}
+})(SongDetail);
